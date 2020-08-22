5c7fd003185463b0f3adc7b957bf6eec8ec8fe6c
Travis CI <travis>
2020-08-22 19:18:57 +0530

    send json data

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name: 'Utsav'},
+    {name: 'Ladani'}
+]
+
+app.get('/users', (req, res) => {
+    res.json({
+        success: true,
+        message: 'successfully done!',
+        users: mockUserData
+    })
+})
+
 app.listen(8000, function () {
     console.log("server is running")
 })
