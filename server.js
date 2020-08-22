19f901a5bbe7b3425ea7eb71051f7784e6c0e908
Travis CI <travis>
2020-08-22 19:42:33 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const bodyParser = reqiure('body-parser');
 const app = express();
+
+app.use(bodyParser.json())
 
 const mockUserData = [
     {name: 'Utsav'},
     {name: 'Ladani'}
 ]
+
+const username = 'Utsav';
+const password = 'Utsav';
 
 app.get('/users', (req, res) => {
     res.json({
@@ -20,6 +26,21 @@ app.get('/users-:id', (req, res) => {
         message: 'Dynamic GET route',
         user: req.params.id
     })
+})
+
+app.post('/login', (req, res) => {
+    if (req.body.username == username && req.body.password == password) {
+        res.json({
+            success: true,
+            message: 'login successfuly'
+        })
+    }
+    else {
+        res.json({
+            success: false,
+            message: 'login failed'
+        })
+    }
 })
 
 app.listen(8000, function () {
